6732e723f4fddbc840488c94227ba7e6ed532088
YourGitHubUsername <abhishekbiradar2856@gmail.com>
2025-07-26 00:36:10 +0530

    chore: update dependencies and add firebase packages
    - Added firebase and firebase-admin to dependencies
    - Updated next to version 15.3.5
    - Updated devDependencies: added @eslint/eslintrc and @tailwindcss/postcss
    - Ensured typescript and tailwindcss are included in devDependencies

---
+++ firebase/client.ts
@@ -0,0 +1,38 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp,getApps } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+import { Auth, getAuth } from "firebase/auth";
+import { FirebaseStorage, getStorage } from "firebase/storage";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyC67Q0LssU8m02bEcjB1xrM70gfgEx0NxE",
+  authDomain: "next-homes-99ccb.firebaseapp.com",
+  projectId: "next-homes-99ccb",
+  storageBucket: "next-homes-99ccb.firebasestorage.app",
+  messagingSenderId: "180112732764",
+  appId: "1:180112732764:web:de502b13ef9c6cea0cac99",
+  measurementId: "G-28C06802LL",
+};
+
+// Initialize Firebase
+const currentApps = getApps()
+let auth:Auth
+let storage:FirebaseStorage
+
+if(!currentApps.length){
+    const app = initializeApp(firebaseConfig)
+    auth = getAuth(app)
+    storage = getStorage(app)
+}else{
+const app = currentApps[0]
+auth = getAuth(app);
+storage = getStorage(app);
+}
+
+
+export {auth,storage}
+//const analytics = getAnalytics(app);
