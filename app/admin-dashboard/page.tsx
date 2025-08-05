07408504c2d9b3494d0571c7e4ca13611cf4df85
YourGitHubUsername <abhishekbiradar2856@gmail.com>
2025-08-05 16:05:46 +0530

    feat: implement layout and breadcrumb components for admin dashboard

--- app/admin-dashboard/page.tsx
+++ app/admin-dashboard/page.tsx
@@ -1,8 +1,20 @@
+import { Breadcrumbs } from "@/components/ui/breadcrumb";
+import { Button } from "@/components/ui/button";
+import { PlusCircle } from "lucide-react";
+import Link from "next/link";
 
 export default function AdminDashboard(){
     return(
         <div>
-            admin-dashboard
+           <Breadcrumbs items={[{
+            label : "Dashboard"
+           }]}/>
+
+          <h1 className="text-4xl font-bold mt-6">Admin Dashboard</h1>
+
+          <Button asChild className="inline-flex  pl-2 gap-2 mt-4 " >
+<Link href="/admin-dashboard/new"> <PlusCircle/> New Property</Link>
+          </Button>
         </div>
     )
 }
